2841ae49d448e3d9a52c9de6282bd3022545e787
jahangirripon <jripon1991@gmail.com>
2025-02-19 20:20:00 +0600

    Rendered list and created Item component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,8 +29,23 @@ function Form() {
 function PackingList() {
   return (
     <div className="list">
-      <h3>What do you need for your trip?</h3>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
     </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❎</button>
+    </li>
   );
 }
 
