4a605f1dd1d5a018f8457487f8822f2cca85a551
jahangirripon <jripon1991@gmail.com>
2025-03-01 09:33:47 +0600

    created and pushed addItems functions in parent state to access in packingList component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import FlashCard from "./FlashCard";
-import DCounter from "./DCounter";
+import App from "./App";
+// import FlashCard from "./FlashCard";
+// import DCounter from "./DCounter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <FlashCard /> */}
-    <DCounter />
+    {/* <DCounter /> */}
   </React.StrictMode>
 );
