7277749faa4379e58e1986270ec8a12486e74c5a
jahangirripon <jripon1991@gmail.com>
2025-03-19 19:08:50 +0600

    completed accordion structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./accStyle.css";
 import App from "./components/App";
 // import FlashCard from "./FlashCard";
 // import DCounter from "./DCounter";
+import Accordion from "./components/Accordion";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Accordion />
     {/* <FlashCard /> */}
     {/* <DCounter /> */}
   </React.StrictMode>
